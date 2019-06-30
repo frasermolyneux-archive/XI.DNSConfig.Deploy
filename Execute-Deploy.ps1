a4b33a575bcb1feb3a426135400b882f71f9c067
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2019-06-30 17:46:18 +0100

    Adding fix for where clause

--- Execute-Deploy.ps1
+++ Execute-Deploy.ps1
@@ -49,7 +49,7 @@ $dnsConfiguration | ForEach-Object {
 
         Write-Information "Processing DNS entry [$($expectedDnsEntry.type)] $($expectedDnsEntry.name)"
 
-        $actualDnsEntry = $actualDnsEntries | Where-Object {$_.name -eq $expectedDnsEntry.name}
+        $actualDnsEntry = $actualDnsEntries | Where-Object {$_.type -eq $expectedDnsEntry.type -and $_.name -eq $expectedDnsEntry.name}
 
         if ($null -eq $actualDnsEntry) {
             Write-Information "DNS entry [$($expectedDnsEntry.type)] $($expectedDnsEntry.name) does not exist in zone $zoneName"
