1b89eec0acd5fa2172d5ccd308bada00973e51c5
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2019-06-30 17:09:47 +0100

    Updated xtremeidiots.net config

--- Config/xtremeidiots.net.ps1
+++ Config/xtremeidiots.net.ps1
@@ -4,21 +4,48 @@ $dnsConfiguration = @(
         "DnsEntries" = @(
             @{
                 "type" = "A"
-                "name" = "powershelltest1.xtremeidiots.net"
-                "content" = "127.0.0.1"
-                "ttl" = 1
-            }
-            @{
-                "type" = "CNAME"
-                "name" = "powershelltest2.xtremeidiots.net"
-                "content" = "google2.co.uk"
+                "name" = "ftp.xtremeidiots.net"
+                "content" = "Execute-Deploy.ps1"
                 "ttl" = 1
             }
             @{
                 "type" = "A"
-                "name" = "gfer.xtremeidiots.net"
-                "content" = "127.0.0.1"
+                "name" = "minecraft-ftp.xtremeidiots.net"
+                "content" = "144.217.255.171"
                 "ttl" = 1
+            }
+            @{
+                "type" = "A"
+                "name" = "minecraft-rdp.xtremeidiots.net"
+                "content" = "144.217.255.171"
+                "ttl" = 1
+            }
+            @{
+                "type" = "A"
+                "name" = "multicraft.xtremeidiots.net"
+                "content" = "144.217.255.171"
+                "ttl" = 1
+                "proxied" = $true
+            }
+            @{
+                "type" = "A"
+                "name" = "redirect.xtremeidiots.net"
+                "content" = "144.217.22.232"
+                "ttl" = 1
+                "proxied" = $true
+            }
+            @{
+                "type" = "CNAME"
+                "name" = "sourcebans-mysql.xtremeidiots.net"
+                "content" = "aa1uqn2xdm4ol0m.cs2um88u8ax2.us-east-2.rds.amazonaws.com"
+                "ttl" = 1
+            }
+            @{
+                "type" = "CNAME"
+                "name" = "sourcebans.xtremeidiots.net"
+                "content" = "xtremeidiotssourcebans-prd.qkpc3nsgzp.us-east-2.elasticbeanstalk.com"
+                "ttl" = 1
+                "proxied" = $true
             }
         )
     }
